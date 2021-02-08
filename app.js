aa457901213afb8450eb4f7ad10dc40ea5a04047
Jenn <hashbrowns120@gmail.com>
2021-02-07 22:47:38 -0600

    basic styling added

---
+++ app.js
@@ -0,0 +1,15 @@
+//players object - factory
+
+const newPlayer = (username) => {
+    return {username};
+}
+
+//gameboard object - module
+const board = (() => {
+
+})();
+
+//game flow object - module
+const gameplay = (() => {
+
+})();
